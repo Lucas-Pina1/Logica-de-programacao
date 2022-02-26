7d6188152c18e890e6a3727bd5f4c21192cfbbc6
Lucas Pina <ti.lucaspina@gmail.com>
2022-02-26 14:39:20 -0300

    curso logica
    operadores

---
+++ inicio/desafio_relacionais.js
@@ -0,0 +1,17 @@
+let a = 1;
+let b = 2;
+
+
+//true
+console.log(a < b);
+console.log(b > a);
+console.log(a != b);
+console.log(a <= b);
+console.log(b >= a);
+
+//false
+console.log(a > b);
+console.log(b < a);
+console.log(b != b);
+console.log(a == b);
+console.log(a === b);
---
+++ operadores/desafio_atribuicao.js
@@ -0,0 +1,17 @@
+
+// let lista = 'Lista de Aprovados\n******************'
+// let ana = '1. Ana Silva'
+// let pedro = '2. Pedro Alburquerque'
+// let guilherme = '3. Guilherme Pereira'
+// let rebenca = '4. Rebeca França'
+// let texto = lista + '\n\n' + ana + '\n' + pedro + '\n' + guilherme + '\n' + rebenca;
+
+let texto = 'LISTA DE APROVADOS\n';
+texto += '******************\n\n';
+texto += '1. Ana Silva\n';
+texto += '2. Pedro Alburquerque\n';
+texto += '3. Guilherme Pereira\n';
+texto += '4. Rebeca França\n';
+
+
+console.log(texto);
